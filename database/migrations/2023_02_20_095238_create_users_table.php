aa95d9d220e41d844bbed84ff538e9356968348b
widyanandaaa <maszidan341@gmail.com>
2023-03-12 21:04:47 +0700

    fixed migrations

--- database/migrations/2023_02_20_095238_create_users_table.php
+++ database/migrations/2023_02_20_095238_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password', 255);
             $table->bigInteger('phone_number');
             $table->string('address', 255);
-            $table->enum('role', ['Pustakawan', 'Member']);
+            $table->enum('role', ['Admin', 'Pustakawan', 'Member']);
             $table->rememberToken();
             $table->timestamps();
         });
